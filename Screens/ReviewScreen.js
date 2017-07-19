d9eefa4c6b69340078b0c4267447c0502a281e13
luteh <luthfanmaftuh@gmail.com>
2017-07-19 08:32:47 +0700

    94

--- Screens/ReviewScreen.js
+++ Screens/ReviewScreen.js
@@ -1,14 +1,23 @@
 /**
  * Created by Luteh on 13/07/2017.
  */
-import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-import {Button} from 'react-native-elements'
+import React, {Component} from "react";
+import {Platform, Text, View} from "react-native";
+import {Button} from "react-native-elements";
 
 class ReviewScreen extends Component {
     static navigationOptions = ({navigation}) => ({
         headerTitle: 'Review Jobs',
-        headerRight: <Button title='Settings' onPress={() => {navigation.navigate('settings')}}/>
+        headerRight:
+            <Button
+                title='Settings'
+                onPress={() => {navigation.navigate('settings')}}
+                backgroundColor='rgba(0,0,0,0)'
+                color="rgba(0,122,255,1)"
+            />,
+        headerStyle:{
+            marginTop: Platform.OS === 'android' ? 24 : 0
+        }
     });
 
     render() {
--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -3,17 +3,22 @@
  */
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
+import Slides from '../components/Slides'
 
-class WelcomeScreen extends Component{
-    render(){
-        return(
-            <View>
-                <Text>WelcomeScreen</Text>
-                <Text>WelcomeScreen</Text>
-                <Text>WelcomeScreen</Text>
-                <Text>WelcomeScreen</Text>
-                <Text>WelcomeScreen</Text>
-            </View>
+const SLIDE_DATA = [
+    {text: 'Welcome to JobApp', color: '#03A9F4'},
+    {text: 'Use this to get a job', color: '#009688'},
+    {text: 'Set your location then swipe away', color: '#03A9F4'}
+];
+
+class WelcomeScreen extends Component {
+    onSlidesComplete(){
+        this.props.navigation.navigate('auth')
+    };
+
+    render() {
+        return (
+            <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete.bind(this)}/>
         )
     }
 }
