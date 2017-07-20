54947fcaa7b583fe80916467b1e796fb8805800d
luteh <luthfanmaftuh@gmail.com>
2017-07-20 11:17:11 +0700

    99. crazy async await syntax

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,6 +1,11 @@
 import {AsyncStorage} from 'react-native'
 import {FACEBOOK_LOGIN_SUCCESS} from './types'
 
-export const facebookLogin = () => {
-
+export const facebookLogin = () => async dispatch => {
+    let token = await AsyncStorage.getItem('fb_token');
+    if (token) {
+// Dispatch an action saying FB  login is done
+    } else {
+// Start up FB Login process
+    }
 };
