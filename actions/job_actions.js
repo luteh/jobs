9bf4c3c1477712f75d697bd71a0a272bcae4ebdb
luteh <luthfanmaftuh@gmail.com>
2017-07-24 11:42:04 +0700

    116. Fetching a list of jobs

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -3,6 +3,8 @@ import qs from 'qs'
 import reverseGeocode from 'latlng-to-zip'
 import {FETCH_JOBS} from './types'
 
+// turn into url save query string using qs library (query string)
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
     publisher: '4201738803816157',
     format: 'json',
@@ -12,9 +14,20 @@ const JOB_QUERY_PARAMS = {
     q: 'javascript'
 };
 
+//to take some location or zipcode from the action creator then formup into long URL
+const buildJobsUrl = (zip) => {
+    const query = qs.stringify({...JOB_QUERY_PARAMS, l: zip});
+    return `${JOB_ROOT_URL}${query}`
+};
+
 export const fetchJobs = (region) => async (dispatch) => {
     try {
-        let zip = await reverseGeocode(region)
+        // Use to take an input latitude longitude and output is zipcode
+        let zip = await reverseGeocode(region);
+        const url = buildJobsUrl(zip);
+        let {data} = await axios.get(url);
+        dispatch({type: FETCH_JOBS, payload: data});
+        console.log(data)
     } catch (e) {
         console.error(e)
     }
