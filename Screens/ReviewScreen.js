36fd395b71445d975f8b9373436d8d423f401ca8
luteh <luthfanmaftuh@gmail.com>
2017-07-27 10:41:17 +0700

    131. Rendering a list of liked jobs

--- Screens/ReviewScreen.js
+++ Screens/ReviewScreen.js
@@ -2,8 +2,8 @@
  * Created by Luteh on 13/07/2017.
  */
 import React, {Component} from "react";
-import {Platform, Text, View} from "react-native";
-import {Button} from "react-native-elements";
+import {Platform, Text, View, ScrollView} from "react-native";
+import {Button, Card} from "react-native-elements";
 import {connect} from 'react-redux'
 
 class ReviewScreen extends Component {
@@ -23,18 +23,42 @@ class ReviewScreen extends Component {
         }
     });
 
+    renderLikedJobs() {
+        // Map over the lists of all the different likes job which has been passing as props
+        return this.props.likedJobs.map(job => {
+            return (
+                <Card>
+                    <View style={{height: 200}}>
+                        <View style={styles.detailWrapper}>
+                            <Text style={styles.italics}>{job.company}</Text>
+                            <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+                        </View>
+                    </View>
+                </Card>
+            )
+        })
+    }
+
     render() {
         return (
-            <View>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-                <Text>ReviewScreen</Text>
-            </View>
+            <ScrollView>
+                {this.renderLikedJobs()}
+            </ScrollView>
         )
     }
 }
+
+const styles = {
+    italics: {
+        fontStyle: 'italic'
+    },
+    detailWrapper: {
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+};
 
 function mapStateToProps(state) {
     return {likedJobs: state.likedJobs}
