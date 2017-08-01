1192b299fce8224c4554cbf2febea6b5cdfec5e9
luteh <luthfanmaftuh@gmail.com>
2017-08-01 12:02:07 +0700

    Implemented redux-persist to handle if terdapat any datas on Review screen, then the user close/restart the app, then the datas will not be gone

--- reducers/likes_reducers.js
+++ reducers/likes_reducers.js
@@ -1,8 +1,11 @@
 import _ from 'lodash'
+import {REHYDRATE} from 'redux-persist/constants'
 import {LIKE_JOB, CLEAR_LIKED_JOBS} from '../actions/types'
 
 export default function (state = [], actions) {
     switch (actions.type) {
+        case REHYDRATE:
+            return actions.payload.likedJobs || [];
         case LIKE_JOB:
             // Handle, if user liking the jobs but the jobs existed on review screen, then the same jobs just show one
             return _.uniqBy([
--- store/index.js
+++ store/index.js
@@ -1,14 +1,24 @@
 import {createStore, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
+import {autoRehydrate, persistStore} from 'redux-persist'
+import {AsyncStorage} from 'react-native'
 
 const store = createStore(
     reducers,
     {}
     ,
     compose(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk),
+        autoRehydrate()
     )
 );
+
+/*
+storage object = whenever redux states changes, store them into storage (AsyncStorage)
+whitelist object = fetch particular reducer, in this case is 'likedJobs
+*/
+
+persistStore(store, {storage: AsyncStorage, whitelist: ['likedJobs']});
 
 export default store
